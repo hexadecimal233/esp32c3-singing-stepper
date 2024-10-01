16d299515b75b216efefb358b956577ae7cda0d7
Hexadecimal233 <me@onlyra1n.top>
2024-10-01 18:11:38 +0800

    basic functionality

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,45 +1,55 @@
-// 这里定义你的输出针脚，注意刷写模式要设置成DIO哦（不然不让跑）
-#define DIR 18  // GPIO 18
-#define PUL 19  // GPIO 19
+
 
 #define PULSE_PER_REV 200  // 决定多少个脉冲转360度 (预留)
 
 #define CHANNEL_NUM 1 //电机数
 
 struct Channel {
-  bool playing; // 是否正在播放
-  float freq; // 音高频率
   int dir_pin; // 方向脚
   int pul_pin; // 脉冲脚
+  int pwm_channel; // PWM控制线程
+  bool direction; // 电机转动方向
   
   // 初始化针脚
-  void init() {
-    playing = false;
-    freq = 440;
+  void init(int pwm_ch) {
+    pwm_channel = pwm_ch;
     
-    if (dir_pin != 0)  {
-      // 设定电机旋转方向
-      pinMode(dir_pin, OUTPUT);
-      digitalWrite(dir_pin, LOW);
-    }
+    pinMode(dir_pin, OUTPUT);
     pinMode(pul_pin, OUTPUT);
+    
+    set_direction(false);
+    
+    ledcSetup(pwm_channel, 2000, 8);
+    ledcAttachPin(pul_pin, pwm_channel);
+  }
+
+
+  // 设定电机旋转方向
+  void set_direction(bool dir) {
+    direction = dir;
+    digitalWrite(dir_pin, dir ? HIGH : LOW);
+  }
+  
+  void reverse() {
+    set_direction(!direction);
   }
 };
 
 Channel* channels = new Channel[CHANNEL_NUM];
 
 void setup() {
+  // 这里定义你的输出针脚，注意刷写模式要设置成DIO哦（不然不让跑）
   channels[0] = {
-    .dir_pin = DIR,
-    .pul_pin = PUL
+    .dir_pin = 18, // GPIO 18
+    .pul_pin = 19 // GPIO 19
   };
 
-  // 打开通信串口 波特率9600
-  Serial.begin(9600);
+  // 打开通信串口 波特率 115200
+  Serial.begin(115200);
 
   // 初始化电机
   for (int i = 0; i < CHANNEL_NUM; i++) {
-    channels[i].init();
+    channels[i].init(i);
   }
 
   Serial.println(F("Singing Motor by hexadecimal233!"));
@@ -51,11 +61,15 @@ void update_status() {
     int packet_id = Serial.parseInt();
     switch (packet_id) {
       case 0: { // 播放音符
-        float freq = Serial.parseFloat();
         int index = Serial.parseInt();
+        float freq = Serial.parseFloat();
+        int reverse = Serial.parseInt();
         Channel& channel = channels[index];
-        channel.freq = freq;
-        channel.playing = true;
+
+        if (freq <= 0) break;
+
+        ledcWriteTone(channel.pwm_channel, freq);
+        if (reverse) channel.reverse();
         
         Serial.print(F("Received play: channel="));
         Serial.print(index);
@@ -66,7 +80,8 @@ void update_status() {
       case 1: { // 停止音符
         int index = Serial.parseInt();
         Channel& channel = channels[index];
-        channel.playing = false;
+        
+        ledcWrite(channel.pwm_channel, 0);
 
         Serial.print(F("Received stop: channel="));
         Serial.println(index);
@@ -76,20 +91,6 @@ void update_status() {
   }
 }
 
-// 播放音符
-void play_note(Channel &channel) {
-  if (channel.freq <= 0 || !channel.playing) return;
-  int pulse_delay = 1000 * 1000 / (channel.freq * 2);  // microseconds between pulses (half-period)
-  digitalWrite(channel.pul_pin, HIGH);
-  delayMicroseconds(pulse_delay);
-  digitalWrite(channel.pul_pin, LOW);
-  delayMicroseconds(pulse_delay);
-  update_status();
-}
-
 void loop() {
   update_status();
-  for (int i = 0; i < CHANNEL_NUM; i++) {
-    play_note(channels[i]);
-  }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,97 @@
-#![allow(unused)]
-
+use std::collections::HashMap;
 use std::time::Duration;
 
-mod midi;
+use midly::{num::u7, MidiMessage, Smf};
+use nodi::midly;
+use nodi::{midly::Format, timers::Ticker, Connection, MidiEvent, Player, Sheet};
+use serialport::SerialPort;
 
 fn main() {
-    let mut port = serialport::new("COM5", 9_600)
-        .timeout(Duration::from_millis(10))
-        .open()
-        .expect("Failed to open port");
+    let file = "here.mid"; // MIDI文件
+    let port = "COM5"; // 串口名称
 
-    print!("成功打开串口");
-    
-    let packet_id_play: u8 = 0;
-    let freq: f32 = 888.0;
-    let channel_index_play: u8 = 0;
+    let midi_data = std::fs::read(file).unwrap();
+    let Smf { header, tracks } = &Smf::parse(&midi_data).unwrap();
 
-    let play_data = format!("{} {} {}\n", packet_id_play, freq, channel_index_play);
-    port.write(play_data.as_bytes()).expect("写入数据失败");
+    let timer = Ticker::try_from(header.timing).unwrap();
+
+    let con = MyConnection::new(port);
+    let sheet = match header.format {
+        Format::SingleTrack | Format::Sequential => Sheet::sequential(&tracks),
+        Format::Parallel => Sheet::parallel(&tracks),
+    };
+
+    let mut player = Player::new(timer, con);
+    player.play(&sheet);
+}
+
+// 音高转频率
+fn freq_from_midi_key(key: i32) -> f32 {
+    440.0 * 2.0f32.powf((key - 69) as f32 / 12.0)
+}
+
+struct MyConnection {
+    port: Box<dyn SerialPort>,  // 串口
+    pressed: HashMap<i32, i32>, // <channel, key>，用来防止电机已经在转的情况下有别的note打断
+}
+
+impl Connection for MyConnection {
+    fn play(&mut self, msg: MidiEvent) -> bool {
+        match msg.message {
+            MidiMessage::NoteOn { key, vel } => {
+                if vel == 0 {
+                    self.stop_note(0);
+                } else {
+                    self.play_note(0, key, false);
+                }
+            }
+            MidiMessage::NoteOff { key, vel } => {
+                self.stop_note(0);
+            }
+            _ => {}
+        }
+
+        true
+    }
+}
+
+impl MyConnection {
+    fn new(port_name: &str) -> MyConnection {
+        MyConnection {
+            port: serialport::new(port_name, 115_200)
+                .timeout(Duration::from_millis(10))
+                .open()
+                .expect("Failed to open port"),
+            pressed: HashMap::new(),
+        }
+    }
+
+    // 播放音符，参数分别是：电机编号，音高，是否让电机反转
+    fn play_note(&mut self, channel: i32, key: u7, reverse: bool) {
+        if self.pressed.contains_key(&channel) {
+            return;
+        }
+        
+        let keyy = u8::from(key) as i32;
+        let play_data = format!(
+            "{} {} {} {}\n",
+            0,
+            channel,
+            freq_from_midi_key(keyy),
+            if reverse { "1" } else { "0" }
+        );
+        self.port
+            .write(play_data.as_bytes())
+            .expect("Error writing");
+        self.pressed.insert(channel, keyy);
+    }
+
+    // 停止音符
+    fn stop_note(&mut self, channel: i32) {
+        let play_data = format!("{} {}\n", 1, channel);
+        self.port
+            .write(play_data.as_bytes())
+            .expect("Error writing");
+        self.pressed.remove(&channel);
+    }
 }
